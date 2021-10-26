3109337c7ce0e3549f4099344dce8f7d3a572ca5
rtribaldos <579671+tribal2@users.noreply.github.com>
2021-10-26 10:32:49 -0500

    chore: add debug build script

---
+++ scripts/debug-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
+source $SCRIPT_DIR/../.config.env
+
+TAG=debug-$($SCRIPT_DIR/getTag.sh)
+
+echo "Building: ${TAG}"
+
+docker build \
+  --build-arg ALPINE_VERSION=${ALPINE_VERSION} \
+  --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
+  -t tribal2/alpine-deployer:${TAG} \
+  $SCRIPT_DIR/../
